42b412b8291f8c6e3e6a54448fa7ac4f63fff974
Björn Wallner <bjornwallner@gmail.com>
2018-05-02 12:15:47 +0200

    test

---
+++ notebooks/intro.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# 
+
+# In[1]:
+
+from sklearn import datasets
+iris = datasets.load_iris()
+digits = datasets.load_digits()
+
+
+# In[2]:
+
+print(digits.data)
+
+
+# In[3]:
+
+print(digits.target)
+
+
